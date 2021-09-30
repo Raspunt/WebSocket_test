71fb93601d1920dcb3d2b16ea2ed5d39cf3b978a
Raspunt <m_ryabinin@inbox.ru>
2021-09-30 11:53:46 +0600

    first commit

---
+++ integers/consumers.py
@@ -0,0 +1,14 @@
+from channels.generic.websocket import WebsocketConsumer
+import json
+from random import randint 
+
+
+class WSConsumer(WebsocketConsumer):
+    
+    def connect(self):
+        self.accept()
+
+
+        for i in range(1000):
+            self.send(json.dumps({'message':randint(1,100)}))
+            
